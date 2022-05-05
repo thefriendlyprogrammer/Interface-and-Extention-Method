27a376907df074d47b74cc4630bb82b3547080db
Alok Kushwaha <95715756+thefriendlyprogrammer@users.noreply.github.com>
2022-05-05 17:46:35 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Interface
+{
+
+    interface interface2
+    {
+        void msg();
+    }
+
+    interface interface1
+    {
+        
+        void msg();
+    }
+
+    static class ext // extention method should be static 
+    {
+        public static void Extention(this interface1 p)// Extention methon does not require permision
+        {
+            Console.WriteLine("Extention is called");
+        }
+    }
+
+    class Test : interface1, interface2
+    {
+        void interface2.msg()
+        {
+            Console.WriteLine("Class test");
+        }
+       
+        void interface1.msg()
+        {
+            throw new NotImplementedException();
+        }
+        static void Main()
+        {
+            Test obj=new Test();
+
+            obj.Extention();
+
+            Console.ReadLine();
+        }
+    }
+}
